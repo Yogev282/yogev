4e7198cbdb210cc27c95cd4172ad7e95fcbee188
Yogev282 <yogev282000@gmail.com>
2022-11-23 15:27:28 +0200

    add1

--- basicClassification.c
+++ basicClassification.c
@@ -1,36 +1,18 @@
 #include <stdio.h>
 #include "NumClass.h"
 
-int findGcd(int a, int b){
-    int gcd = 0;
-    int l = 0;
-    if(a<b){
-        l = a;
-    }
-    else {
-        l = b;
-    }
-    for(int i = 1; i < l ; i++){
-        if( (a % i == 0) && (b % i == 0) ){
-            gcd = i;
-        }
-    }
-    return gcd;
-}
-
-
 int isPrime(int k){
-    if(k==0 || k==1){
+    if(k==0){
         return 0;
     }
-    if(k == 2){
+    if(k == 2 || k == 1){
         return 1;
     }
     
-    for(int i = 2 ; i <= k ; i++){
-        if(findGcd(k,i) != 1){
-            return 0;
-        }
+    for(int i = 2 ; i < k ; i++){
+       if(k%i == 0){
+        return 0;
+       }
     }
     return 1;
 }
